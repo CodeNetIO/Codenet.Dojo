0f8bbf1e395510c2b7ca4b5a4172d0c20240395e
darinh <darin@codenet.io>
2015-08-13 14:42:21 -0700

    Added a unit test for compilers.

---
+++ Codenet.Dojo.Compilers.Tests/StringCompilerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codenet.Dojo.Compilers.Tests
+{
+    [TestClass]
+    public class StringCompilerTests
+    {
+        private static string SIMPLE_STATIC_METHOD = @"
+            using System;
+
+            public static class SimpleStatic
+            {
+                public static string GetAString()
+                {
+                    return ""A String"";
+                }
+            }
+            ";
+
+        [TestMethod]
+        public void StringCompiler_SimpleStaticMethod()
+        {
+            var stringCompiler = new StringCompiler();
+            var assembly = stringCompiler.Compile(SIMPLE_STATIC_METHOD);
+            Assert.IsNotNull(assembly);
+
+            var exportedType = assembly.ExportedTypes.FirstOrDefault();
+            Assert.IsNotNull(exportedType);
+            Assert.AreEqual("SimpleStatic", exportedType.Name);
+
+            var getAStringMethod = exportedType.GetMethod("GetAString");
+            Assert.IsNotNull(getAStringMethod);
+            Assert.AreEqual("A String", getAStringMethod.Invoke(null,null));
+        }
+    }
+}
