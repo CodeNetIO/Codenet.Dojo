129b1020f4726196a3dca02102d9ac298eb58a16
darinh <darin@codenet.io>
2015-08-12 21:52:34 -0700

    Wrote SimplStaticTest.

--- Codenet.Dojo.Services.Tests/DojoServiceTests.cs
+++ Codenet.Dojo.Services.Tests/DojoServiceTests.cs
@@ -6,9 +6,39 @@ namespace Codenet.Dojo.Services.Tests
     [TestClass]
     public class DojoServiceTests
     {
+
+        private static string SIMPLE_STATIC_METHOD = @"
+            using System;
+
+            public static class SimpleStatic
+            {
+                public static string GetAString()
+                {
+                    return ""A String"";
+                }
+            }
+            ";
+
+        private static string SIMPLE_STATIC_METHOD_TEST = @"
+            using System;
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class SimpleStaticTests
+            {
+                [TestMethod]
+                public void SimpleStatic_GetAString()
+                {
+                    Assert.AreEqual(""A String"", SimpleStatic.GetAString());
+                }
+            }
+            ";
+
         [TestMethod]
-        public void HappyTest()
+        public void ProcessSimple_StaticMethod()
         {
+            var service = new DojoService();
+            Assert.AreEqual("Success", service.ProcessSimple(SIMPLE_STATIC_METHOD, SIMPLE_STATIC_METHOD_TEST));
         }
     }
 }
